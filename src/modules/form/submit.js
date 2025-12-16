a7eb95736e875ef5161f0f1b3ddb7e27cb2563fd
João Victor <joaovictorsalotialves12@gmail.com>
2025-12-15 22:44:07 -0300

    Definindo a data atual

---
+++ src/modules/form/submit.js
@@ -0,0 +1,18 @@
+import dayjs from 'dayjs'
+
+const form = document.querySelector('form')
+const selectDate = document.getElementById('date')
+
+const inputToday = dayjs().format('DD/MM/YYYY')
+
+// Carregra a data atual no campo de data
+selectDate.value = inputToday
+
+// Define que a data mínima selecionável é a data atual
+selectDate.min = inputToday
+
+form.onsubmit = (event) => {
+  event.preventDefault()
+
+  console.log('Formulário enviado!')
+}
