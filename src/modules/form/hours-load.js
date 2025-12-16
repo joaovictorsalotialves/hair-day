7121ddae7fceccf286c1ed92d151a085e8b54a6a
João Victor <joaovictorsalotialves12@gmail.com>
2025-12-16 13:14:20 -0300

    Bloqueando horários já agendados

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -4,19 +4,26 @@ import { hoursClick } from './hours-click.js'
 
 const hours = document.querySelector('.hours')
 
-export function hoursLoad({ date }) {
+export function hoursLoad({ date, dailySchedules }) {
   // Limpa a lista de horários
   hours.innerHTML = ''
+
+  // Obtem as horas já agendadas
+  const unavailableHours = dailySchedules.map(
+    schedule => dayjs(schedule.when).format('HH:mm')
+  )
 
   const opening = openingHours.map((hour) => {
     const [scheduleHour, _] = hour.split(':')
 
     // Adiciona a hora na data e verifica se já passou
-    const isHourPast = dayjs(date).add(scheduleHour, 'hour').isAfter(dayjs())
+    const isHourPast = dayjs(date).add(scheduleHour, 'hour').isBefore(dayjs())
+
+    const available = !unavailableHours.includes(hour) && !isHourPast
 
     return {
       hour, 
-      available: isHourPast,
+      available,
     }
   })
 
