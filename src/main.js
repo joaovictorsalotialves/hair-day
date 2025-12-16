98fd5d252f22ed9f74d668dd08d091396b2d2096
João Victor <joaovictorsalotialves12@gmail.com>
2025-12-15 23:19:56 -0300

    Carregando os horários

--- src/main.js
+++ src/main.js
@@ -9,5 +9,6 @@ import './styles/form.css'
 import './styles/schedule.css'
 
 // JS
-import './modules/load.js'
+import './modules/page-load.js'
 import './modules/form/submit.js'
+import './modules/schedules/load.js'
---
+++ src/modules/schedules/load.js
@@ -0,0 +1,9 @@
+import { hoursLoad } from '../form/hours-load.js'
+
+const selectedDate = document.getElementById('date')
+
+export function schedulesDay() {
+  const date = selectedDate.value
+  // Renderiza as horas disponíveis
+  hoursLoad({ date })
+}
